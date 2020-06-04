8d757071efef66b2f6ec35624ab934c43d01c8ff
Ahmet Sina Ustem <asend.ustem@gmail.com>
2020-06-04 13:57:21 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,14 @@ import PackageDescription
 let package = Package(
     name: "MediaBrowser",
     platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
+    products: [
+        .library(name: "MediaBrowser", targets: ["MediaBrowser"])
+    ],
     targets: [
         .target(
             name: "MediaBrowser",
             path: "",
+            dependencies: ["SDWebImage", "UICircularProgressRing"]
         ),
     ],
     swiftLanguageVersions: [.v5]

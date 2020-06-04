4f4fc5df40f843d41a26263e360796203e989b8e
Ahmet Sina Ustem <asend.ustem@gmail.com>
2020-06-04 13:53:50 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "MediaBrowser"
+    name: "MediaBrowser",
+    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
+    targets: [
+        .target(
+            name: "MediaBrowser",
+            path: "",
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )

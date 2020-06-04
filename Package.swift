15bf8879212edfeb41d1acfa38af8ee32abe1bf1
Ahmet Sina Ustem <asend.ustem@gmail.com>
2020-06-04 14:07:29 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,10 @@ let package = Package(
     products: [
         .library(name: "MediaBrowser", targets: ["MediaBrowser"])
     ],
-    dependencies: [ "SDWebImage", "UICircularProgressRing"],
+    dependencies: [ 
+        .package(url: "https://github.com/luispadron/UICircularProgressRing.git", from: "6.5.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.0")
+    ],
     targets: [
         .target(
             name: "MediaBrowser",
